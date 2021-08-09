552171e2204152c3b90e6aed5010f1aa1000b960
waqarRns <waqar@rnssol.com>
2021-08-09 09:17:11 +0500

    blacklist error fixed

--- src/modules/middleware/blacklistMiddleware.ts
+++ src/modules/middleware/blacklistMiddleware.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from 'express';
-import { logger, Logger } from '../common/Logger';
+import { Request, Response, NextFunction } from "express";
+import { logger, Logger } from "../common/Logger";
 
 /**
  * Middleware to check the blacklist IP
@@ -7,14 +7,13 @@ import { logger, Logger } from '../common/Logger';
  */
 export const isBlackList = async (req: Request, res: Response, next: NextFunction) => {
     let ipAddress = String(req.ip);
-    let db = Logger.dbInstance.connection.db;
-    if (!db)
-        next();
-    let isBlackListed = await db.collection('blacklists').findOne({ ipAddress });
-    if (!isBlackListed)
-        next();
+    let db = Logger.dbInstance;
+    if (!db) return next();
+    db = db.connection.db;
+    let isBlackListed = await db.collection("blacklists").findOne({ ipAddress });
+    if (!isBlackListed) next();
     else {
-        res.status(403).send('Your request has been rejected.')
-        logger.warn(`Error: IP:${ipAddress} is blocked.`)
+        res.status(403).send("Your request has been rejected.");
+        logger.warn(`Error: IP:${ipAddress} is blocked.`);
     }
-}
+};
