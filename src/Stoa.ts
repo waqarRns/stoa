e4f45cc9eaa9f527534ceb6a512df442fb9c3f86
Ahmed Tariq <ahmed@rnssol.com>
2021-10-08 15:53:11 +0500

    Validator ballot API was added

--- src/Stoa.ts
+++ src/Stoa.ts
@@ -73,7 +73,8 @@ import {
     IPendingProposal,
     IProposalAPI,
     IProposalList,
-    IVotingDetails
+    IVotingDetails,
+    IBallotAPI
 } from "./Types";
 
 class Stoa extends WebService {
@@ -287,7 +288,7 @@ class Stoa extends WebService {
         this.app.get("/proposal/:proposal_id", isBlackList, this.getProposalById.bind(this));
         this.app.get("/validator/reward/:address", isBlackList, this.getValidatorReward.bind(this));
         this.app.get("/proposal/voting_details/:proposal_id", isBlackList, this.getVotingDetails.bind(this));
-
+        this.app.get("/validator/ballot/:address", isBlackList, this.getValidatorBallots.bind(this));
 
         // It operates on a private port
         this.private_app.post("/block_externalized", this.postBlock.bind(this));
@@ -2793,7 +2794,6 @@ class Stoa extends WebService {
      */
     public async getValidatorReward(req: express.Request, res: express.Response) {
         const address = String(req.params.address);
-
         let validatorAddress: PublicKey;
         try {
             validatorAddress = new PublicKey(address);
@@ -2802,8 +2802,7 @@ class Stoa extends WebService {
             return;
         }
         const pagination: IPagination = await this.paginate(req, res);
-        this.ledger_storage
-            .getValidatorReward(address, pagination.pageSize, pagination.page)
+        this.ledger_storage.getValidatorReward(address, pagination.pageSize, pagination.page)
             .then((data: any[]) => {
                 if (data.length === 0) {
                     return res.status(204).send(`The data does not exist.`);
@@ -2820,6 +2819,50 @@ class Stoa extends WebService {
                         });
                     }
                     return res.status(200).send(JSON.stringify(rewards));
+                }
+            })
+            .catch((err) => {
+                logger.error("Failed to data lookup to the DB: " + err, {
+                    operation: Operation.db,
+                    height: HeightManager.height.toString(),
+                    status: Status.Error,
+                    responseTime: Number(moment().utc().unix() * 1000),
+                });
+                return res.status(500).send("Failed to data lookup");
+            });
+    }
+    /* Get validator ballots
+     * @returns Returns BOA Holder of the ledger.
+     */
+    public async getValidatorBallots(req: express.Request, res: express.Response) {
+        const address = String(req.params.address);
+        let validatorAddress: PublicKey;
+        try {
+            validatorAddress = new PublicKey(address);
+        } catch (error) {
+            res.status(400).send(`Invalid value for parameter 'address': ${address}`);
+            return;
+        }
+        const pagination: IPagination = await this.paginate(req, res);
+        this.ledger_storage
+            .getValidatorBallots(address, pagination.pageSize, pagination.page)
+            .then((data: any[]) => {
+                if (data.length === 0) {
+                    return res.status(204).send(`The data does not exist.`);
+                } else {
+                    const ballots: IBallotAPI[] = [];
+                    for (const row of data) {
+                        ballots.push({
+                            proposal_id: row.proposal_id,
+                            tx_hash: new Hash(row.tx_hash, Endian.Little).toString(),
+                            sequence: row.sequence,
+                            proposal_type: ConvertTypes.ProposalTypetoString(row.proposal_type),
+                            proposal_title: row.proposal_title,
+                            ballot_answer: ConvertTypes.ballotAddressToString(row.ballot_answer),
+                            full_count: row.full_count
+                        });
+                    }
+                    return res.status(200).send(JSON.stringify(ballots));
                 }
             })
             .catch((err) => {
